2b5c82ec6a4641197017373476d4b9b7abaaaf80
AstaYC PS <baihmohamedamine@gmail.com>
2024-12-16 23:04:06 +0100

    let's go to the first step in the file rouge

---
+++ src/main/java/com/astayc/hontodare/HontoDareApplication.java
@@ -0,0 +1,13 @@
+package com.astayc.hontodare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HontoDareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HontoDareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/astayc/hontodare/HontoDareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astayc.hontodare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HontoDareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
