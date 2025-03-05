f4f677148d1e44ec5fad4d9e1fabf45cdee78146
AstaYC PS <baihmohamedamine@gmail.com>
2025-03-05 17:45:36 +0000

    i finished the creation of two method in webS contoller

--- backend/src/main/java/com/astayc/hontodare/Config/WebSocket/WebSocketEventListener.java
+++ backend/src/main/java/com/astayc/hontodare/Config/WebSocket/WebSocketEventListener.java
@@ -1,8 +1,12 @@
 package com.astayc.hontodare.Config.WebSocket;
 
+import com.astayc.hontodare.Chat.ChatMessage;
+import com.astayc.hontodare.Chat.Enum.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -11,8 +15,20 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @RequiredArgsConstructor
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations messagingTemplate;
+
     @EventListener
     public void HandleWebSocketDisConnectListener(SessionDisconnectEvent event) {
-//        TODO
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+
+        if(username != null) {
+            log.info("User Disconnected : " + username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messagingTemplate.convertAndSend("/topic/chat", chatMessage);
+        }
     }
 }
